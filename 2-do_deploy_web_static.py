66f4b5eef6ce5d2e418d6418abf72cce8a1fe552
Swiftminale <minalefetene@gmail.com>
2024-05-04 13:23:59 -0700

    first commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['100.27.5.70', '54.174.44.244']
 
 
 def do_deploy(archive_path):
